00d0664eb477b860dcc8e7db31e3c9d2a72480ae
Dario Carlomagno <dario.carlomagno@gmail.com>
2016-11-06 15:44:38 +0100

    [abracapp] - fix push JS syntax

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
   push: {
     ios: {
       pfx: process.env.PFX || '', // The filename of private key and certificate in PFX or PKCS12 format from disk  
