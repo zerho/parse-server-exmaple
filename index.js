6da415539a6b047373f3500e70cfdcb4bbe632e2
Dario Carlomagno <dario.carlomagno@gmail.com>
2016-11-06 15:33:28 +0100

    [abracapp] - modified index.js

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ var api = new ParseServer({
       bundleId: process.env.BUNDLE_ID || '', // The bundle identifier associate with your app
       production: process.env.PUSH_PRODUCTION || false // Specifies which environment to connect to: Production (if true) or Sandbox
     }
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
