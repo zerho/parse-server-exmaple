053a6f0ed5812dcf8200220f6f58312c8491ab32
Dario Carlomagno <dario.carlomagno@gmail.com>
2016-11-21 19:13:14 +0100

    [abracapp] - fix lines

--- cloud/main.js
+++ cloud/main.js
@@ -5,26 +5,26 @@ Parse.Cloud.afterSave("Posts", function(request) {
   var commentRelation = aPost.relation("comments");
   var commentRelationQuery = commentRelation.query();
   commentRelationQuery.descending("createdAt");
-  commentRelationQuery.limit(1)
   commentRelationQuery.find().then(function(results) {
 
-  var aComment = results[0]
-  var query = new Parse.Query('_Parse.Installation');
-  var postOwner = "ABRA_User_" + userOwner.id
-  query.equalTo('channels', postOwner);
-  Parse.Push.send({
-    where: query,
-    data: { 
-      "title": "abracapp",
-      "alert": userOwner.get("username") + ":" + aComment.get("text") + "in: " + aPost.get("text")
-    }
-    }, { useMasterKey: true }).then(() => {
-        // Push was successful
-        console.log('DAJEEEEEE weeee!');
-    }, (e) => {
-        console.log('ERRORE: ' + e);
+    var aComment = results[0]
+    var query = new Parse.Query('_Parse.Installation');
+    var postOwner = "ABRA_User_" + userOwner.id
+    query.equalTo('channels', postOwner);
+
+    Parse.Push.send({
+      where: query,
+      data: { 
+        "title": "abracapp",
+        "alert": userOwner.get("username") + ":" + aComment.get("text") + "in: " + aPost.get("text")
+      }
+      }, { useMasterKey: true }).then(() => {
+          // Push was successful
+          console.log('DAJEEEEEE weeee!');
+      }, (e) => {
+          console.log('ERRORE: ' + e);
+      });
     });
-  });
 
 });
 
