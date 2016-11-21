e5128cf67141aa5694c56efce57365d2425a9fdb
Dario Carlomagno <dario.carlomagno@gmail.com>
2016-11-21 17:51:15 +0100

    [abracapp] - upload first trial of a push

--- cloud/main.js
+++ cloud/main.js
@@ -1,31 +1,22 @@
 
-// Array.prototype.last = function() {
-//     return this[this.length-1];
-// }
-
-// Parse.Cloud.afterSave("Post", function(request) {
-// var aPost = request.object.extend("Post");
-// var commentRelation = subjectObj.relation("comments");
-// var aComment = commentRelation.last()
-// var query = new Parse.Query('_Parse.Installation');
-// var postOwner = "ABRA_User_" + aPost.get('user').id
-// query.equalTo('channels', postOwner);
-// Parse.Push.send({
-// 	where: query,
-// 	data: { 
-// 		"title": "abracapp",
-// 		"alert": request.user.get('username') + "likes your post: " + aPost.get('text')
-// 	}
-// 	}, { useMasterKey: true });
-// });
+Array.prototype.last = function() {
+    return this[this.length-1];
+}
 
 Parse.Cloud.afterSave("Posts", function(request) {
-	Parse.Push.send({
-    where: new Parse.Query(Parse.Installation), // Set our Installation query
-    data: {
-      alert: "New message: Ciao a tutti"
-    }
-  }, { useMasterKey: true}).then(() => {
+var aPost = request.object.extend("Post");
+var commentRelation = subjectObj.relation("comments");
+var aComment = commentRelation.last()
+var query = new Parse.Query('_Parse.Installation');
+var postOwner = "ABRA_User_" + aPost.get('user').id
+query.equalTo('channels', postOwner);
+Parse.Push.send({
+	where: query,
+	data: { 
+		"title": "abracapp",
+		"alert": request.user.get('username') + : + aComment.get('text') + "in: " + aPost.get('text')
+	}
+	}, { useMasterKey: true }).then(() => {
       // Push was successful
       console.log('DAJEEEEEE weeee!');
   }, (e) => {
