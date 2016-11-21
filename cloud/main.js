2c9aaece6bebe4169105dde27ab8b431a51ab349
Dario Carlomagno <dario.carlomagno@gmail.com>
2016-11-21 18:26:36 +0100

    [abracapp] - setting console log

--- cloud/main.js
+++ cloud/main.js
@@ -1,12 +1,13 @@
 Parse.Cloud.afterSave("Posts", function(request) {
-Array.prototype.last = function() {
-  return this[this.length-1];
-}
 var aPost = request.object;
 var commentRelation = aPost.relation("comments");
 var aComment = commentRelation[commentRelation.length - 1].fetch();
 var query = new Parse.Query('_Parse.Installation');
 var postOwner = "ABRA_User_" + aPost.get('user').id
+console.log('VAR: ' + postOwner);
+console.log('VAR: ' + request.user.get('username'));
+console.log('VAR: ' + aComment.get('text');
+console.log('VAR: ' + aPost.get('text');
 query.equalTo('channels', postOwner);
 Parse.Push.send({
   where: query,
