870f7ed4d80c93ab08ce9ec82f4a8667c575b9d4
Dario Carlomagno <dario.carlomagno@gmail.com>
2016-11-21 19:04:40 +0100

    [abracapp] - trying to find relation

--- cloud/main.js
+++ cloud/main.js
@@ -1,19 +1,31 @@
 Parse.Cloud.afterSave("Posts", function(request) {
-var aPost = request.object;
-var commentRelation = aPost.relation("comments");
-var query = new Parse.Query('_Parse.Installation');
-var postOwner = "ABRA_User_" + aPost.get('user').id
-query.equalTo('channels', postOwner);
-Parse.Push.send({
-  where: query,
-  data: { 
-    "title": "abracapp",
-    "alert": aPost.get("user").get("username") + ":" + "prova commento" + "in: " + aPost.get("text")
-  }
-  }, { useMasterKey: true }).then(() => {
-      // Push was successful
-      console.log('DAJEEEEEE weeee!');
-  }, (e) => {
-      console.log('ERRORE: ' + e);
+  var aPost = request.object;
+  var userOwner = aPost.get("user").fetch()
+
+  var commentRelation = aPost.relation("comments");
+  var commentRelationQuery = commentRelation.query();
+  commentRelationQuery.descending("createdAt");
+  commentRelationQuery.limit(1)
+  commentRelationQuery.find().then(function(results) {
+
+  var aComment = results[0]
+  var query = new Parse.Query('_Parse.Installation');
+  var postOwner = "ABRA_User_" + userOwner.id
+  query.equalTo('channels', postOwner);
+  Parse.Push.send({
+    where: query,
+    data: { 
+      "title": "abracapp",
+      "alert": userOwner.get("username") + ":" + aComment.get("text") + "in: " + aPost.get("text")
+    }
+    }, { useMasterKey: true }).then(() => {
+        // Push was successful
+        console.log('DAJEEEEEE weeee!');
+    }, (e) => {
+        console.log('ERRORE: ' + e);
+    });
   });
+
 });
+
+
