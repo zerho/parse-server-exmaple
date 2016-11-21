caf30056d086ceae8780863cc267c2fff4b719eb
Dario Carlomagno <dario.carlomagno@gmail.com>
2016-11-21 16:53:45 +0100

    [abracapp] - modified cloud code file

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,20 @@
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Array.prototype.last = function() {
+    return this[this.length-1];
+}
+
+Parse.Cloud.afterSave("Post", function(request) {
+var aPost = request.object.extend("Post");
+var commentRelation = subjectObj.relation("comments");
+var aComment = commentRelation.last()
+var query = new Parse.Query('_Parse.Installation');
+var postOwner = "ABRA_User_" + aPost.get('user').id
+query.equalTo('channels', postOwner);
+Parse.Push.send({
+	where: query,
+	data: { 
+		"title": "abracapp",
+		"alert": request.user.get('username') + "likes your post: " + aPost.get('text')
+	}
+	}, { useMasterKey: true });
 });
