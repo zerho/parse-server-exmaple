ecd355b8fc5929471462e96ac43955bae0965c21
Dario Carlomagno <dario.carlomagno@gmail.com>
2016-11-21 19:38:22 +0100

    [abracapp] - just another commit

--- cloud/main.js
+++ cloud/main.js
@@ -1,23 +1,23 @@
 Parse.Cloud.afterSave("Posts", function(request) {
   var aPost = request.object;
-  var userOwner = aPost.get("user").fetch()
 
   var commentRelationQuery = aPost.relation("comments").query();
   commentRelationQuery.descending("createdAt");
+  commentRelationQuery.limit(1)
   commentRelationQuery.find().then(function(results) {
 
     console.log(results)
 
     var aComment = results[0].fetch()
     var query = new Parse.Query('_Parse.Installation');
-    var postOwner = "ABRA_User_" + userOwner.id
+    var postOwner = "ABRA_User_" + aPost.get("user").id
     query.equalTo('channels', postOwner);
 
     Parse.Push.send({
       where: query,
       data: { 
         "title": "abracapp",
-        "alert": userOwner.get("username") + ":" + aComment.get("text") + "in: " + aPost.get("text")
+        "alert": aPost.get("user").get("username") + ":" + aComment.get("text") + "in: " + aPost.get("text")
       }
       }, { useMasterKey: true }).then(() => {
           // Push was successful
