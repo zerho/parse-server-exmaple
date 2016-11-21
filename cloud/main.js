291db839627ea4eab9300009e4e6e190096e406f
Dario Carlomagno <dario.carlomagno@gmail.com>
2016-11-21 17:56:53 +0100

    [abracapp] - modified function

--- cloud/main.js
+++ cloud/main.js
@@ -1,9 +1,7 @@
-
-Array.prototype.last = function() {
-    return this[this.length-1];
-}
-
 Parse.Cloud.afterSave("Posts", function(request) {
+Array.prototype.last = function() {
+  return this[this.length-1];
+}
 var aPost = request.object.extend("Post");
 var commentRelation = subjectObj.relation("comments");
 var aComment = commentRelation.last()
@@ -11,12 +9,12 @@ var query = new Parse.Query('_Parse.Installation');
 var postOwner = "ABRA_User_" + aPost.get('user').id
 query.equalTo('channels', postOwner);
 Parse.Push.send({
-	where: query,
-	data: { 
-		"title": "abracapp",
-		"alert": request.user.get('username') + : + aComment.get('text') + "in: " + aPost.get('text')
-	}
-	}, { useMasterKey: true }).then(() => {
+  where: query,
+  data: { 
+    "title": "abracapp",
+    "alert": request.user.get('username') + ":" + aComment.get('text') + "in: " + aPost.get('text')
+  }
+  }, { useMasterKey: true }).then(() => {
       // Push was successful
       console.log('DAJEEEEEE weeee!');
   }, (e) => {
