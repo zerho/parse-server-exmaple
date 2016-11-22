0aa60f27456565882a6906a6b488dcd10599fc78
Dario Carlomagno <dario.carlomagno@gmail.com>
2016-11-23 00:04:16 +0100

    [abracapp] - some checks

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,11 @@
 Parse.Cloud.define("sendPush", function(request, response) {
+  
+  var query = new Parse.Query('_Parse.Installation');
+  var postOwner = request.params.userChannel
+  var postOwner2 = request.params.userchannel
+  console.log("PROVA 1" + postOwner);
+  console.log("PROVA 2" + postOwner2);
+  query.equalTo('channels', postOwner);
   Parse.Push.send({   
   where: query,
   data: request.params
