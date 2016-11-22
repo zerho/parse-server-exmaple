6993cc738d839470367323ea0bd5cbd5667934e8
Dario Carlomagno <dario.carlomagno@gmail.com>
2016-11-23 00:08:02 +0100

    [abracapp] - fixed cloud code define

--- cloud/main.js
+++ cloud/main.js
@@ -2,9 +2,7 @@ Parse.Cloud.define("sendPush", function(request, response) {
   
   var query = new Parse.Query('_Parse.Installation');
   var postOwner = request.params.userChannel
-  var postOwner2 = request.params.userchannel
-  console.log("PROVA 1" + postOwner);
-  console.log("PROVA 2" + postOwner2);
+  console.log("PROVA 1" + postOwner)
   query.equalTo('channels', postOwner);
   Parse.Push.send({   
   where: query,
