f2670c406dafd39d31f512d2e24521d6345aa9a8
Dario Carlomagno <dario.carlomagno@gmail.com>
2017-02-04 23:38:58 +0000

    [implementation] - save notification object when push delivered

--- cloud/main.js
+++ cloud/main.js
@@ -1,14 +1,27 @@
 Parse.Cloud.define("sendPush", function(request, response) {
-  
-  var query = new Parse.Query('_Parse.Installation');
-  var postOwner = request.params.userChannel
-  query.equalTo('channels', postOwner);
-  Parse.Push.send({   
-  where: query,
-  data: request.params
-  }, { useMasterKey: true }).then(function() {
-                              response.success()
-                            }, function(error) {
-                              response.error(error);
-                            });
+
+    var query = new Parse.Query('_Parse.Installation');
+    var postOwner = request.params.userChannel
+    query.equalTo('channels', postOwner);
+    Parse.Push.send({
+        where: query,
+        data: request.params
+    }, {
+        useMasterKey: true
+    }).then(function() {
+
+        var notifications = Parse.Object.extend("Notifications");
+        var aNotification = new notifications();
+        aNotification.set("postType", request.params.type)
+        aNotification.set("senderUsername", request.params.username)
+        aNotification.set("postObjectId", request.params.postId)
+        aNotification.set("parseObjectId", request.params.kParsePushParameterNotificationParseObjectId)
+        aNotification.set("notificationMessage", request.params.messasge)
+        aNotification.save().then(function() {}, function(error) {
+            response.error(error);
+        });
+
+    }, function(error) {
+        response.error(error);
+    });
 });
